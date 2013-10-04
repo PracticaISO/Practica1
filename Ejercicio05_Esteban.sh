0aa50ee9a2b027a28d5270f6b8c05bbb98a8d74e
PracticaISO <esteban.trgallego@gmail.com>
2013-10-04 08:35:48 +0200

    Update Ejercicio05_Esteban.sh

--- Ejercicio05_Esteban.sh
+++ Ejercicio05_Esteban.sh
@@ -11,7 +11,30 @@ read a
 echo -n "¿En qué dia empieza?(0 es lunes y 6 es domingo): "
 read b
 echo "  L  M  X  J  V  S  D"
-
+if [ $b -eq 1 ]
+then 
+	echo -n "   "
+fi
+if [ $b -eq 2 ]
+then 
+	echo -n "      "
+fi
+if [ $b -eq 3 ]
+then 
+	echo -n "         "
+fi
+if [ $b -eq 4 ]
+then 
+	echo -n "            "
+fi
+if [ $b -eq 5 ]
+then 
+	echo -n "               "
+fi
+if [ $b -eq 6 ]
+then 
+	echo -n "                  "
+fi
 for d in `seq $c $a`
 do 
 	for b in `seq $b 6`
