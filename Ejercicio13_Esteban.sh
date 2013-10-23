d060a5dde998b46fe0c26abe44cf1b1e89987792
PracticaISO <esteban.trgallego@gmail.com>
2013-10-23 09:42:35 +0200

    Update Ejercicio13_Esteban.sh

--- Ejercicio13_Esteban.sh
+++ Ejercicio13_Esteban.sh
@@ -1,1 +1,13 @@
-http://www.cuantarazon.com/741842/sal-de-la-friendzone
+#http://www.cuantarazon.com/741842/sal-de-la-friendzone
+
+#!/bin/bash
+
+echo `cat /etc/passwd | grep "100[0-9]"|cut -f 1 -d :` > user
+echo `cat /etc/passwd | grep "100[0-9]"|cut -f 3 -d :` > id
+cat user
+cat id
+echo `cat user|cut -f 1 -d " "` "	        (" `cat id|cut -f 1 -d " "` ")"
+echo `cat user|cut -f 2 -d " "` "	        (" `cat id|cut -f 2 -d " "` ")"
+echo `cat user|cut -f 3 -d " "` "	( "`cat id|cut -f 3 -d " "` ")"
+echo `cat user|cut -f 4 -d " "` "	        (" `cat id|cut -f 4 -d " "` ")"
+echo `cat user|cut -f 5 -d " "` "	        (" `cat id|cut -f 5 -d " "` ")"
