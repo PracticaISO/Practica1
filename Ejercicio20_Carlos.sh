4ede6aacdccd60ef665dbda6435b71513087fd02
PracticaISO <esteban.trgallego@gmail.com>
2013-10-25 21:05:12 +0200

    Update Ejercicio20_Carlos.sh

--- Ejercicio20_Carlos.sh
+++ Ejercicio20_Carlos.sh
@@ -2,19 +2,19 @@
 
 # ejercico20
 
-nucleos=`cat /proc/cpuinfo | grep processor | wc -l`
-modelo=`cat /proc/cpuinfo | grep "model name" | awk -F:' ' '{ print $2 }'`
+nucleos=`cat /proc/cpuinfo | grep processor | wc -l`							#procesador
+modelo=`cat /proc/cpuinfo | grep "model name" | awk -F:' ' '{ print $2 }'`				#modelo
 
-if cat /proc/cpuinfo | egrep '(vmx|svn)' &>2
+if cat /proc/cpuinfo | egrep '(vmx|svn)' &>2								#virtualizacion
 then
 	virtualizacion="Sí"
 else
 	virtualizacion="No"
 fi
-memorialibrekb=`cat /proc/meminfo | grep "MemFree" | awk -F: '{ print $2 }' | awk '{ print $1 }'`
-let "memorialibremb = memorialibrekb / 1024"
+memorialibrekb=`cat /proc/meminfo | grep "MemFree" | awk -F: '{ print $2 }' | awk '{ print $1 }'`	#memoria
+let "memorialibremb = memorialibrekb / 1024"								#memoria MB
 
-echo "Número de CPUs: $nucleos"
+echo "Número de CPUs: $nucleos"										#imprime resultados
 echo "Modelo de CPU: $modelo"
 echo "Temperatura: $temperatura"
 echo "Virtualización: $virtualizacion"
